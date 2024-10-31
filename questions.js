6951695fca709c6a97f862680a363f7391e407b4
0xde5cafe <manfromxenon@proton.me>
2024-10-30 22:55:34 -0300

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,81 +1,42 @@
-const relevo1 = [
-  "hello this is a test input",
-  "hey whats up",
-  "another string",
-  "yet another string"
+const questions = [
+  {
+    question: "chaves",
+    answers: [
+      "quico.",
+      "nhonho.",
+      "professor girafales",
+      "n.d.a"
+    ]
+  },
+  {
+    question: "marvel",
+    answers: [
+      "capitao america.",
+      "homem arnaha odfnefipn43ioffpfref.",
+      "hulk",
+      "japão"
+    ]
+  },
+  {
+    question: "vamonos de fiesta a factory",
+    answers: [
+      "pero lleva mucho botiquin",
+      "porque mucha gente cae ahi",
+      "pon ahora tu mejor skin",
+      "o relevo desde o surgimento da Terra não sofreu modificações em sua estrutura."
+    ]
+  },
+  // Add more questions and answers here...
 ];
 
-const relevo2 = [
-  "hello this is a test input",
-  "hey whats up",
-  "another string",
-  "yet another string"
-];
+// Select a random question
+const randomQuestion = questions[Math.floor(Math.random() * questions.length)];
 
-const agente1 = [
-  "hello this is a test input",
-  "hey whats up",
-  "another string",
-  "yet another string"
-];
+// Set the random question in the HTML element
+document.getElementById("question").innerHTML = randomQuestion.question;
 
-const agente2 = [
-  "hello this is a test input",
-  "hey whats up",
-  "another string",
-  "yet another string"
-];
-
-const fuso1 = [
-  "hello this is a test input",
-  "hey whats up",
-  "another string",
-  "yet another string"
-];
-
-const fuso2 = [
-  "hello this is a test input",
-  "hey whats up",
-  "another string",
-  "yet another string"
-];
-
-const carto1 = [
-  "hello this is a test input",
-  "hey whats up",
-  "another string",
-  "yet another string"
-];
-
-const carto2 = [
-  "hello this is a test input",
-  "hey whats up",
-  "another string",
-  "yet another string"
-];
-
-const cardeal1 = [
-  "hello this is a test input",
-  "hey whats up",
-  "another string",
-  "yet another string"
-];
-
-const cardeal2 = [
-  "hello this is a test input",
-  "hey whats up",
-  "another string",
-  "yet another string"
-];
-
-// Create an array of all the arrays
-const allArrays = [relevo1, relevo2, agente1, agente2, fuso1, fuso2, carto1, carto2, cardeal1, cardeal2];
-
-// Select a random array
-const randomArray = allArrays[Math.floor(Math.random() * allArrays.length)];
-
-// Select a random string from the random array
-const randomString = randomArray[Math.floor(Math.random() * randomArray.length)];
-
-// Set the random string in the HTML element
-document.getElementById("question").innerHTML = randomString;
+// Set the random answers in the HTML elements
+const buttons = document.querySelectorAll(".button-container button");
+buttons.forEach((button, index) => {
+  button.innerHTML = randomQuestion.answers[index];
+});
